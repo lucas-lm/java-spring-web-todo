43d4ebe2145de1a4ef0a4679dc66e2e8d7a61bfd
Lucas Miranda <lucas-m@outlook.com>
2023-10-14 13:03:01 -0300

    feat(users): add `user already exists` handle for user creation

--- src/main/java/com/lucasm/taskman/user/IUserRepository.java
+++ src/main/java/com/lucasm/taskman/user/IUserRepository.java
@@ -4,4 +4,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<UserModel, UUID> {}
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+}
--- src/main/java/com/lucasm/taskman/user/UserController.java
+++ src/main/java/com/lucasm/taskman/user/UserController.java
@@ -1,6 +1,7 @@
 package com.lucasm.taskman.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,15 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
-    var user = this.userRepository.save(userModel);
-    return user;
+  public ResponseEntity create(@RequestBody UserModel user) {
+    var foundUser = this.userRepository.findByUsername(user.getUsername());
+
+    if (foundUser != null) {
+      System.out.println("User already exists.");
+      return ResponseEntity.badRequest().body("User already exists");
+    }
+
+    var newUser = this.userRepository.save(user);
+    return ResponseEntity.ok(newUser);
   }
 }
