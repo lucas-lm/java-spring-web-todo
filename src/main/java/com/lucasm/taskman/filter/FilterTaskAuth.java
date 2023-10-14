1c5c9b749ed22f84fea3a240e63a53c6efe6f637
Lucas Miranda <lucas-m@outlook.com>
2023-10-14 17:17:20 -0300

    feat(auth): add password verification

--- src/main/java/com/lucasm/taskman/filter/FilterTaskAuth.java
+++ src/main/java/com/lucasm/taskman/filter/FilterTaskAuth.java
@@ -2,9 +2,14 @@ package com.lucasm.taskman.filter;
 
 import java.io.IOException;
 import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.lucasm.taskman.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +18,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
+        // auth parsing
         var authorizationHeader = request.getHeader("Authorization");
         var authPayloadEncoded = authorizationHeader.replace("Basic ", "").trim();
         byte[] rawAuthPayloadDecoded = Base64.getDecoder().decode(authPayloadEncoded);
@@ -25,7 +34,20 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String password = credentials[1];
         System.out.println(username);
         System.out.println(password);
-        filterChain.doFilter(request, response);
+
+        // user validation
+        var user = userRepository.findByUsername(username);
+        if (user == null) {
+          response.sendError(401); // user does not exist
+        } else {
+          var passwordVerification = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+          if (passwordVerification.verified) {
+            filterChain.doFilter(request, response);
+          } else {
+            response.sendError(401);
+          }
+        }
+        System.out.println(user);
   }
   
 }
